a29233d4a4af65b8c51a7c086a2408f5898811fd
Bikash Kumar Singh <bikashsinghak47@gmail.com>
2023-02-16 13:43:42 +0530

    Application Name Change : Done

--- ios/shoesmobileappTests/shoesmobileappTests.m
+++ ios/shoesmobileappTests/shoesmobileappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface aaronfaishonTests : XCTestCase
+@interface shoesmobileappTests : XCTestCase
 
 @end
 
-@implementation aaronfaishonTests
+@implementation shoesmobileappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
